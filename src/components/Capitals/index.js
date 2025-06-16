1f78d4cfcfa785ffda4c89126247033050b17dc8
rakesh <rakeshsagartfi21@gmail.com>
2025-06-16 08:14:58 +0000

    Added Onchange function

--- src/components/Capitals/index.js
+++ src/components/Capitals/index.js
@@ -33,22 +33,41 @@ const countryAndCapitalsList = [
 // Write your code here
 
 class Capitals extends Component {
+  state = {
+    activeCapitalId: countryAndCapitalsList[0].id
+  }
+
+  onChangeCapital = (event) =>{
+    this.setState({activeCapitalId: event.target.value})
+  }
+
+  getCountry = () =>{
+    const {activeCapitalId} = this.state
+    const activeCountryAndCapital = countryAndCapitalsList.find(eachCapital => (
+      eachCapital.id === activeCapitalId
+    ))
+    return activeCountryAndCapital.country
+  }
+
   render() {
+    const {activeCapitalId} = this.state 
+    const country = this.getCountry(activeCapitalId)
+  
     return (
       <div className="app-container">
         <div className="card-container">
           <h1 className="heading"> Countries And Capitals </h1>
           <div className="question-container">
-            <select className="capital-select">
+            <select className="capital-select" onChange={this.onChangeCapital} value={activeCapitalId}>
               {countryAndCapitalsList.map(eachCapital => (
-                <option key={eachCapital.id} className="option">
+                <option key={eachCapital.id} className="option" value={eachCapital.id}>
                   {eachCapital.capitalDisplayText}
                 </option>
               ))}
             </select>
             <p className="question"> is capital of which country? </p>
           </div>
-          <p className="country"> India </p>
+          <p className="country"> {country} </p>
         </div>
       </div>
     )
